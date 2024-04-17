0087eea9b0252f4ad022d2d4c67a958cdb7dc7d8
Krishnan-Karthika <krish17karthi@gmail.com>
2024-04-17 22:28:50 +0530

    two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Project2';
-  mrngMsg: string = "Good Morning";
-  noonMsg: string = "Good Afternoon";
-  displayMsg: string = "";
-  evenOrOdd: string = "even";
+  data:string = "test string";
+  intValue:number = 0;
+  tempVal:number=0;
+  output:string="even";
 
-  showMrngMsg() {
-    this.displayMsg = ", "+this.mrngMsg;
-  }
-  showNoonMsg() {
-    this.displayMsg = ", "+this.noonMsg;
-  }
-
-  count = 0;
-  incrementFn() {
-    this.count++;
-    this.showNature();
-  }
-  decrementFn() {
-    this.count--;
-    this.showNature();
-  }
-  showNature() {
-    this.evenOrOdd = this.count%2 == 0 ? "even" : "odd";
+  checkValue() {
+    this.tempVal = this.intValue;
+    this.output = this.intValue%2 == 0 ? "even" : "odd";
   }
 }
